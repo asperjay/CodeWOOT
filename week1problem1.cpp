11e3789b8af06f1c635900aa0890ba2aa378a486
asperjay <jasperelsley@gmail.com>
2023-09-11 16:20:19 -0700

    j

--- week1problem1.cpp
+++ week1problem1.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-template<typename T, std::size_t S>
-void swim(std::vector<int>& heap, int currentIndex) {
-    int parentIndex = (currentIndex - 1) / 2;
+void swim(std::vector<int>& heap, int childIndex) {
+    int parentIndex = (childIndex - 1) / 2;
 
-    while (c) {}
-        std::swap(heap[currentIndex], heap[parentIndex]);
-        currentIndex = parentIndex;
-        parentIndex = (currentIndex - 1) / 2;
+    while (childIndex > 0 && heap[parentIndex] > heap[childIndex]) {
+        std::swap(heap[childIndex], heap[parentIndex]);
+        childIndex = parentIndex;
+        parentIndex = (childIndex - 1) / 2;
     }
 }
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!" << std::endl;
+    std::vector<int> heap = {4,1,2};
+    heap.push_back(3);
+    for (size_t i = 0; i < heap.size(); i++) {
+        std::cout << heap[i] << " ";
+    }
+    std::cout << std::endl;
+    swim(heap,3);
+    for (size_t i = 0; i < heap.size(); i++) {
+        std::cout << heap[i] << " ";
+    }
     return 0;
 }
