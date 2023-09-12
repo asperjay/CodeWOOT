a3c1ee812fbbac5f85598134e6ad20f16064e945
asperjay <jasperelsley@gmail.com>
2023-09-11 18:16:09 -0700

    finished algorithms

--- week1problem1.cpp
+++ week1problem1.cpp
@@ -4,23 +4,47 @@ using namespace std;
 void swim(std::vector<int>& heap, int childIndex) {
     int parentIndex = (childIndex - 1) / 2;
 
-    while (childIndex > 0 && heap[parentIndex] > heap[childIndex]) {
+    while (childIndex > 0 && heap[parentIndex] < heap[childIndex]) {
         std::swap(heap[childIndex], heap[parentIndex]);
         childIndex = parentIndex;
         parentIndex = (childIndex - 1) / 2;
     }
 }
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    std::vector<int> heap = {4,1,2};
-    heap.push_back(3);
+void sink(std::vector<int>& heap, int parentIndex = 0) {
+    int childIndex = 2 * parentIndex + 1;
+
+    while (parentIndex < heap.size() && heap[parentIndex] < heap[childIndex]) {
+        std::swap(heap[childIndex], heap[parentIndex]);
+        parentIndex = childIndex;
+        childIndex = 2 * parentIndex + 1;
+    }
+}
+void processInstruction(std::vector<int>& heap, int instruction) {
+    if (instruction == 0) {
+        int last = heap.back();
+        heap.pop_back();    
+        heap[0] = last;
+        sink(heap);
+    } else {
+        heap.push_back(instruction);
+        swim(heap,heap.size()-1);
+    }
+}
+void printHeap(std::vector<int>& heap) {
     for (size_t i = 0; i < heap.size(); i++) {
         std::cout << heap[i] << " ";
     }
     std::cout << std::endl;
-    swim(heap,3);
-    for (size_t i = 0; i < heap.size(); i++) {
-        std::cout << heap[i] << " ";
-    }
+}
+int main() {
+    std::vector<int> heap;
+    heap.push_back(5);
+    heap.push_back(2);
+    heap.push_back(3);
+    printHeap(heap);
+    processInstruction(heap,4);
+    printHeap(heap);
+    processInstruction(heap,0);
+    printHeap(heap);
     return 0;
 }
